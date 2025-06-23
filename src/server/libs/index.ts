c7b4360336c18f5133f5cefab2a7b1575aacc08f
Bhupendra <15957214+BhupenT@users.noreply.github.com>
2025-06-23 23:40:48 +1000

    - exported default middleware

--- src/server/libs/index.ts
+++ src/server/libs/index.ts
@@ -3,4 +3,5 @@ export * from './btpsFactory.js';
 export * from './btpsRegistry.js';
 export * from './btpsSimpleMetricsTracker.js';
 export * from './btpsSimpleRateLimiter.js';
+export * from './defaultMiddleware.js';
 export * from './type.js';
