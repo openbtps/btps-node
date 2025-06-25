0ed0f5bb7cc3b5ec96a227028bc68892a5f71c12
Bhupendra <15957214+BhupenT@users.noreply.github.com>
2025-06-25 23:28:26 +1000

    - added test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,14 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    coverage: {
+      provider: 'v8', // or 'istanbul' if preferred
+      reporter: ['text', 'html', 'json'],
+      reportsDirectory: './coverage',
+      all: true, // include files without tests too
+      include: ['src/**/*.ts'], // adjust as needed
+      exclude: ['**/*.d.ts', '**/types.ts', '**/constant.ts'],
+    },
     globals: true,
     environment: 'node',
   },
